b90b48a9e282eab471842b6eaa55d477aba83d78
Andreia Costa <aandreia_a.c@msn.com>
2022-09-23 00:08:34 -0300

    sorting funcionality created

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,16 +1,77 @@
+import { useState } from 'react';
 import '../styles/product-list.css';
 const ProductList = ({ products, addToCart }) => {
+
+  const [productsList, setProductList] = useState(products)
+
+  const handleOrderList = () => {
+    let newProducts = [...products]
+
+    let selectOption = document.getElementById("selectBox");
+    let selectedValue = selectOption.options[selectOption.selectedIndex].value;
+
+    if (selectedValue === '2') {
+      newProducts.sort((a,b) => {
+        if (a.price > b.price) {
+          return 1
+        }
+        else {
+          if (a.price < b.price) {
+            return -1;
+          }
+          else {
+            return 0;
+          }
+        }
+      })
+    }
+
+    else if (selectedValue === '3') {
+      newProducts.sort((a,b) => {
+        if (a.percent < b.percent) {
+          return 1
+        }
+        else {
+          if (a.percent > b.percent) {
+            return -1;
+          }
+          else {
+            return 0;
+          }
+        }
+      })
+    }
+
+    else if (selectedValue === '4') {
+      newProducts.sort((a,b) => {
+        if (a.rating < b.rating) {
+          return 1
+        }
+        else {
+          if (a.rating > b.rating) {
+            return -1;
+          }
+          else {
+            return 0;
+          }
+        }
+      })
+    }
+    
+
+    setProductList(newProducts)
+  }
+
   return (
     <section className="section all-products" id="products">
       <div className="top container">
         <h1>Todos os Produtos</h1>
         <form>
-          <select>
+          <select id="selectBox" onChange={handleOrderList}>
             <option value="1">Default Sorting</option>
             <option value="2">Sort By Price</option>
-            <option value="3">Sort By Popularity</option>
-            <option value="4">Sort By Sale</option>
-            <option value="5">Sort By Rating</option>
+            <option value="3">Sort By Sale</option>
+            <option value="4">Sort By Rating</option>
           </select>
           <span>
             <i className="bx bx-chevron-down"></i>
@@ -18,7 +79,7 @@ const ProductList = ({ products, addToCart }) => {
         </form>
       </div>
       <div className="product-center container">
-        {products.map((product) => (
+        {productsList.map((product) => (
           <div className="product-item">
             <div className="overlay">
               <a href="/" className="product-thumb">
@@ -27,7 +88,8 @@ const ProductList = ({ products, addToCart }) => {
                   alt="Imagem do produto"
                 />
               </a>
-              <span className="discount">40%</span>
+              <span className="discount"
+                    style={!product.percent ? { display: "none" } : undefined}>{`-${product.percent}%`}</span>
             </div>
             <div className="product-info">
               <div className="product-stars">
