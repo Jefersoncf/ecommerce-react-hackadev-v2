b2e5ee22a605fc5250246bdeaf1f921f795d04a9
Jeferson Ferreira <fjeferson60@gmail.com>
2022-10-03 16:56:50 -0300

    increasing slide height

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,11 +2,7 @@ import Footer from '../components/Footer';
 import NavBar from '../components/NavBar';
 import About from '../components/About';
 
-
-import { useDispatch, useSelector } from 'react-redux';
-
 const AboutPage = () => {
-  
   return (
     <>
       <NavBar></NavBar>
