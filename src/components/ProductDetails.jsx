aef8e124a4c90d846284301da32e55fefc05d9c2
Jeferson Ferreira <fjeferson60@gmail.com>
2022-10-24 07:10:25 -0300

    Add description to size and change quantity in ProductDetails

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,45 +1,55 @@
-import { Link } from 'react-router-dom';
-import '../styles/product-details.css';
+import { Link } from "react-router-dom";
+import "../styles/product-details.css";
 
-const ProductDetails = ({ product, addToCart }) => {
+const ProductDetails = ({ product, addToCart, quanity }) => {
   return (
     <section className="section product-detail">
       <div className="details container-b">
         <div className="left image-container">
           <div className="main">
-            <img src={`/images/product${product.id_product}.png`} alt="Imagem do produto" />
+            <img
+              src={`/images/product${product.id_product}.png`}
+              alt="Imagem do produto"
+            />
           </div>
         </div>
         <div className="right">
           <span>{product.category_name}</span>
           <h1>{product.product_name}</h1>
-          <p className="rating">Avaliações: {[...Array(product.product_rating)].map(() => (
-                                                      <i className="bx bxs-star"></i>
-                                            ))}
+          <p className="rating">
+            Avaliações:{" "}
+            {[...Array(product.product_rating)].map(() => (
+              <i className="bx bxs-star"></i>
+            ))}
           </p>
           <div className="price">
-            <p>Preço: {product.product_price.toLocaleString('pt-br', {
-                                            style: 'currency',
-                                            currency: 'BRL',
-                      })}
+            <p>
+              Preço:{" "}
+              {product.product_price.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
             </p>
           </div>
           <form>
-            {product.product_size
-            ?
-            <div>
+            <label htmlFor="">Tamanho:</label>{" "}
+            {product.product_size ? (
+              <div>
                 {[...Array(product.product_size.length)].map(() => (
-                    <div>{product.size_name}</div>
+                  <div>{product.size_name}</div>
                 ))}
-            </div>
-            : 
-            <div></div>
-            }
-            
+              </div>
+            ) : (
+              <div></div>
+            )}
           </form>
           <form className="form">
-            <input type="number" value="1"/>
-            <button type="button" className="addCart" onClick={() => addToCart(product)}>
+            <input type="number" value={product.quanity} />
+            <button
+              type="button"
+              className="addCart"
+              onClick={() => addToCart(product)}
+            >
               Comprar
             </button>
             <Link to="/cart" className="addCart">
